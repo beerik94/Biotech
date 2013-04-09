8c8aef0217d2de3e130cff022488a79f51fdc5b4
liquidgithub <liquidgithub@hotmail.com>
2013-04-09 14:24:03 +0200

    Worked some more on getting my Cheese Rendered.

--- src/minecraft/biotech/client/ItemRenderHandler.java
+++ src/minecraft/biotech/client/ItemRenderHandler.java
@@ -1,34 +1,47 @@
 package biotech.client;
 
-import org.lwjgl.opengl.GL11;
-
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.IItemRenderer;
+import biotech.client.model.ModelCheese;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class ItemRenderHandler implements IItemRenderer
 {
-
+	public ModelCheese	cheese	= new ModelCheese();
+	
 	@Override
 	public boolean handleRenderType(ItemStack item, ItemRenderType type)
 	{
-		return true;
+		switch (type)
+		{
+			case EQUIPPED:
+				return true;
+			default:
+				return false;
+		}
 	}
-
+	
 	@Override
 	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper)
 	{
-		return true;
+		return false;
 	}
-
+	
 	@Override
 	public void renderItem(ItemRenderType type, ItemStack item, Object... data)
 	{
-		if(type == ItemRenderType.INVENTORY)
-        {
-        	GL11.glTranslatef(1F, 1F, 1F);
-        }
-		
-		
+		switch (type)
+		{
+			case EQUIPPED:
+			{
+				
+			}
+			default:
+				break;
+		}
 	}
 	
 }
