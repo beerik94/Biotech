38c99efce775ad2b524852a6071a86dc5d00f45a
Erik <liquidgithub@hotmail.com>
2013-04-09 20:26:06 +0200

    Can't seem to get the BioCheese OBJ to render.

--- src/minecraft/biotech/client/model/ModelCheese.java
+++ src/minecraft/biotech/client/model/ModelCheese.java
@@ -15,15 +15,26 @@ public class ModelCheese extends ModelBase
 
     public ModelCheese()
     {
-        this.cheese = new NMTModelRenderer(this);
-        this.cheese.addModelOBJ(Biotech.ModelBioCheese);
+    	this.textureHeight = 512;
+    	this.textureWidth = 512;
+    	
+        cheese = new NMTModelRenderer(this);
+        cheese.setTextureSize(512, 512);
+        cheese.mirror = true;
+        cheese.addModelOBJ(Biotech.ModelBioCheese);
     }
     
     /**
      * Sets the models various rotation angles then renders the model.
      */
-    public void render(Entity par1Entity, float par2, float par3, float par4, float par5, float par6, float par7)
+    public void render(Entity Entity, float par2, float par3, float par4, float par5, float par6, float par7)
     {
-        this.cheese.render(par7);
+    	super.render(Entity, par2, par3, par4, par5, par6, par7);
+        cheese.render(par7);
+    }
+    
+    public void setRotationAngles(float par1, float par2, float par3, float par4, float par5, float par6, Entity entity)
+    {
+    	super.setRotationAngles(par1, par2, par3, par4, par5, par6, entity);
     }
 }
