a03672bdfc602582a5e7c1ee6812c2077a1af6de
Jesus <dude4455@hotmail.com>
2013-05-15 20:22:18 -0600

    Code Cleanup
    Just Cleaning Code

--- src/minecraft/biotech/slots/slotRangeUpgrade.java
+++ src/minecraft/biotech/slots/slotRangeUpgrade.java
@@ -12,7 +12,6 @@ public class slotRangeUpgrade extends Slot
 	public slotRangeUpgrade(IInventory par1iInventory, int par2, int par3, int par4)
 	{
 		super(par1iInventory, par2, par3, par4);
-		
 	}
 	
 	@Override
