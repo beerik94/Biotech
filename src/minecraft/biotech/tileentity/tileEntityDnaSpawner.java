472b9285c641ac9dbab1e090ec855a12ff806548
DarkGuardsman <rseifert.phone@gmail.com>
2013-07-13 14:29:22 -0400

    remove rewritten entities

--- src/minecraft/biotech/tileentity/tileEntityDnaSpawner.java
+++ src/minecraft/biotech/tileentity/tileEntityDnaSpawner.java
@@ -1,31 +1,27 @@
 package biotech.tileentity;
 
 import net.minecraft.entity.Entity;
-import biotech.entity.passive.bioChicken;
-import biotech.entity.passive.bioCow;
-import biotech.entity.passive.bioPig;
-import biotech.entity.passive.bioSheep;
 
 public class tileEntityDnaSpawner extends tileEntityBasicMachine
 {
 	// Watts being used per cut
-	public static final double	ENERGY_PER_ACTION	= 100;
-	
+	public static final double ENERGY_PER_ACTION = 100;
+
 	// Stats
-	public int					health;
-	public float				width;
-	public float				height;
-	public int					drops;
-	public int					EV;
-	
-	public boolean				buttonSpawn			= false;
-	private int					proccessTicks		= 0;
-	
+	public int health;
+	public float width;
+	public float height;
+	public int drops;
+	public int EV;
+
+	public boolean buttonSpawn = false;
+	private int proccessTicks = 0;
+
 	public tileEntityDnaSpawner()
 	{
 		super();
 	}
-	
+
 	@Override
 	public void updateEntity()
 	{
@@ -41,7 +37,7 @@ public class tileEntityDnaSpawner extends tileEntityBasicMachine
 				{
 					proccessTicks++;
 				}
-				if(proccessTicks == 40)
+				if (proccessTicks == 40)
 				{
 					buttonSpawn = false;
 					proccessTicks = 0;
@@ -50,31 +46,35 @@ public class tileEntityDnaSpawner extends tileEntityBasicMachine
 			}
 		}
 	}
-	
+
 	public void SpawnMob(int dnaDamage)
 	{
 		Entity par1Entity;
 		switch (dnaDamage)
 		{
 			case 0:
-				par1Entity = new bioChicken(worldObj, this.xCoord, this.yCoord + 1, this.zCoord, health, width, height, drops, EV);
-				worldObj.spawnEntityInWorld(par1Entity);
+				// par1Entity = new bioChicken(worldObj, this.xCoord, this.yCoord + 1, this.zCoord,
+				// health, width, height, drops, EV);
+				//worldObj.spawnEntityInWorld(par1Entity);
 				break;
 			case 1:
-				par1Entity = new bioCow(worldObj, this.xCoord, this.yCoord + 1, this.zCoord, health, width, height, drops, EV);
-				worldObj.spawnEntityInWorld(par1Entity);
+				// par1Entity = new bioCow(worldObj, this.xCoord, this.yCoord + 1, this.zCoord,
+				// health, width, height, drops, EV);
+				//worldObj.spawnEntityInWorld(par1Entity);
 				break;
 			case 2:
-				par1Entity = new bioPig(worldObj, this.xCoord, this.yCoord + 1, this.zCoord, health, width, height, drops, EV);
-				worldObj.spawnEntityInWorld(par1Entity);
+				// par1Entity = new bioPig(worldObj, this.xCoord, this.yCoord + 1, this.zCoord,
+				// health, width, height, drops, EV);
+				//worldObj.spawnEntityInWorld(par1Entity);
 				break;
 			case 3:
-				par1Entity = new bioSheep(worldObj, this.xCoord, this.yCoord + 1, this.zCoord, health, width, height, drops, EV);
-				worldObj.spawnEntityInWorld(par1Entity);
+				// par1Entity = new bioSheep(worldObj, this.xCoord, this.yCoord + 1, this.zCoord,
+				// health, width, height, drops, EV);
+				//worldObj.spawnEntityInWorld(par1Entity);
 				break;
 		}
 	}
-	
+
 	public void CheckLowestValue()
 	{
 		if (health < 1)
@@ -98,7 +98,7 @@ public class tileEntityDnaSpawner extends tileEntityBasicMachine
 			EV = 1;
 		}
 	}
-	
+
 	@Override
 	public String getInvName()
 	{
