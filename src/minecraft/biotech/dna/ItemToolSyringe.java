a5de64142e87e037a48ebd39b0d851fc5f1006a0
DarkGuardsman <rseifert.phone@gmail.com>
2013-07-13 17:25:24 -0400

    Created an item to get DNA without drops
    Drops just don't seem right for DNA even if minecraft is just a game. As
    well might want to code the item to be called Blood before its actual
    DNA. This way the player has to process it too see the DNA effects.

---
+++ src/minecraft/biotech/dna/ItemToolSyringe.java
@@ -0,0 +1,48 @@
+package biotech.dna;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import biotech.Biotech;
+import biotech.dna.DNARegistry.DNAData;
+
+public class ItemToolSyringe extends Item
+{
+
+	public ItemToolSyringe(int id)
+	{
+		super(id);
+		setCreativeTab(Biotech.tabBiotech);
+		setUnlocalizedName("Syringe");
+		setMaxStackSize(1);
+	}
+
+	@Override
+	public void registerIcons(IconRegister iconRegister)
+	{
+
+	}
+
+	@Override
+	public boolean itemInteractionForEntity(ItemStack par1ItemStack, EntityLiving par2EntityLiving)
+	{
+		DNAData data = DNARegistry.getDNAFor(par2EntityLiving);
+		if (data != null)
+		{
+			int delay = par2EntityLiving.getEntityData().getInteger("dnaDelay");
+			if (delay > 0)
+			{
+				EntityTickHandler.onHarvestDna(par2EntityLiving, delay);
+				return true;
+			}
+			System.out.println("[BioTech]Debug: DNA extracted");
+			System.out.println("[BioTech]DNAData:" + data.toString());
+			EntityTickHandler.onHarvestDna(par2EntityLiving, -1);
+			return true;
+		}
+
+		return false;
+	}
+
+}
